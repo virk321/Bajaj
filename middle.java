012237283eee09e82d4d7ed2232a39d41342febd
Manjot Kaur <156766468+virk321@users.noreply.github.com>
2026-05-11 20:58:32 +0530

    Add files via upload

---
+++ middle.java
@@ -0,0 +1,36 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class middle{
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        ListNode ans = middleNode(a);
+        System.out.println(ans.val);
+    }
+}
